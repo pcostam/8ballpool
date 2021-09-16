d19e02bcc1b0a1fe4e2a571b84f3c9d22cd59ee8
pcostam <36275331+pcostam@users.noreply.github.com>
2021-09-17 00:50:25 +0100

    new app

--- app/build/classes/java/main/pipeline/App.java
+++ app/build/classes/java/main/pipeline/App.java
@@ -3,8 +3,7 @@
  */
 package pipeline;
 
-import kafka.KafkaConfig;
-import kafka.Producer;
+import kafka.*;
 import mongodb.BulkWriteInsert;
 import mongodb.MongoDBSetup;
 
@@ -21,18 +20,23 @@ import org.apache.kafka.common.KafkaFuture;
 public class App {
 
     public static void main(String[] args) throws IOException, ExecutionException, InterruptedException {
+        String topicName = "8sample-topic";
+        int numberOfThread = 3;
+        String groupId = "java-group-consumer";
+
         //setup mongoDb
-        MongoDBSetup setupMongo = new MongoDBSetup();
-        BulkWriteInsert.doBulkInsert(setupMongo.getCollection());
+        /*MongoDBSetup setupMongo = new MongoDBSetup();
+        BulkWriteInsert.doBulkInsert(setupMongo.getCollection());*/
         //initialize zookeeper and kafka server???
 
-        Properties properties = new Properties();
+        /*Properties properties = new Properties();
         properties.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, KafkaConfig.BOOTSTRAP_SERVER);
+
+
 
         try (AdminClient admin = AdminClient.create(properties)) {
             int partitions = 1;
             short replicationFactor = 1;
-            String topicName = "8sample-topic";
 
             ListTopicsOptions options = new ListTopicsOptions();
             options.listInternal(true);
@@ -50,12 +54,34 @@ public class App {
                 future.get();
             }
 
-        }
+        }*/
 
 
         //sends init and in-app events
-        Producer producer = new Producer();
+        /*Producer producer = new Producer();
         Thread t1 = new Thread(producer);
         t1.start();
+
+
+        Consumer consumers = new Consumer(groupId, topicName);
+
+        consumers.execute(numberOfThread);
+
+        consumers.shutdown();*/
+        String outputTopic = "output-topic";
+        KafkaUtils.createNewTopic(outputTopic);
+
+
+        Producer producerStream = new Producer(topicName);
+        Thread t1 = new Thread(producerStream);
+        t1.start();
+
+        Streams streams = new Streams(topicName, outputTopic, "streams-application");
+        streams.doStream(topicName);
+
+        System.out.println(">>>>CONSUMER");
+        Consumer consumerStream = new Consumer(groupId, outputTopic);
+        consumerStream.execute(numberOfThread);
+        consumerStream.shutdown();
     }
 }
