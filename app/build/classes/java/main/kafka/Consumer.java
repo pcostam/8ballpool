b1cd707a9705e7dfe6b7480a3dff1f241d6221d8
pcostam <36275331+pcostam@users.noreply.github.com>
2021-10-18 19:25:20 +0100

    consumes more type of events

--- app/build/classes/java/main/kafka/Consumer.java
+++ app/build/classes/java/main/kafka/Consumer.java
@@ -3,6 +3,7 @@ package kafka;
 import events.Event;
 import events.InApp;
 import events.Init;
+import events.Match;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
@@ -19,6 +20,9 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
+/**
+ * Single consumer, multiple worker processing threads
+ */
 public class Consumer {
     private final KafkaConsumer<String, ? extends Event> consumer;
     private final String topic;
@@ -33,13 +37,11 @@ public class Consumer {
     }
 
     /**
-     * Creates a {@link ThreadPoolExecutor} with a given number of threads to consume the messages
-     * from the broker.
+     * Creates a {@link ThreadPoolExecutor} with a given number of threads to consume the messages.
      *
      * @param numberOfThreads The number of threads will be used to consume the message
      */
     public void execute(int numberOfThreads) {
-        System.out.println(">>>>>>>>>>>>>> execution");
         final Logger logger = LoggerFactory.getLogger(Consumer.class);
         logger.info("Initialize execution");
         // Initialize a ThreadPool with size = 5 and use the BlockingQueue with size =1000 to
@@ -51,17 +53,33 @@ public class Consumer {
         while (true) {
             ConsumerRecords<String, ? extends Event> records = consumer.poll(Duration.ofMillis(100));
             for (ConsumerRecord<String, ? extends Event> record : records) {
-                if(record.value().getEventType().toString().equals("init")) {
-                    System.out.println("is init");
+                if(record.value() instanceof Init) {
                     ConsumerRecord<String, Init> subtypeRecord = (ConsumerRecord<String, Init> ) record;
-                    //received new record
-                    logger.info( "Received new records: " + " key: " + subtypeRecord.key() + " value: " + subtypeRecord.value().getCountry()+ "\n");
+                    //received new record new record type Init
+                    logger.info( "Received new records: " + " key: " + subtypeRecord.key() +
+                                " value country: " + subtypeRecord.value().getCountry() +
+                                " value user id: " + subtypeRecord.value().getUserId()+
+                                " value platform: "+ subtypeRecord.value().getPlatform()+
+                                " value time: " + subtypeRecord.value().getTime()+ "\n");
                 }
-                else {
-                    System.out.println("is in app");
+                else if(record.value() instanceof InApp){
                     ConsumerRecord<String, InApp> subtypeRecord = (ConsumerRecord<String, InApp> ) record;
-                    //received new records
-                    logger.info( "Received new records: " + " key: " + subtypeRecord.key() + " value: " + subtypeRecord.value().getUserId()+ "\n");
+                    //received new record type InApp
+                    logger.info( "Received new records: " + " key: " + subtypeRecord.key() +
+                            " value user id: " + subtypeRecord.value().getUserId()+
+                            " value product id :" + subtypeRecord.value().getProductId()+
+                            " value purchase value:" + subtypeRecord.value().getPurchaseValue()+
+                            " value time: "+ subtypeRecord.value().getTime()+
+                            "\n");
+                }
+
+                else if(record.value() instanceof Match){
+                    ConsumerRecord<String, Match> subtypeRecord = (ConsumerRecord<String, Match> ) record;
+                    //received new record type InApp
+                    logger.info( "Received new records: " + " key: " + subtypeRecord.key() +
+                            " value user id A: " + subtypeRecord.value().getUserA()+
+                            " value user id  B:" + subtypeRecord.value().getUserB()+
+                            "\n");
                 }
 
 
@@ -70,19 +88,27 @@ public class Consumer {
         }
     }
 
+    /**
+     * Create properties configuration for consumer
+     * @param groupId
+     * @return Properties
+     */
     private static Properties createConsumerConfig(String groupId) {
         Properties properties = new Properties();
         properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, KafkaConfig.BOOTSTRAP_SERVER);
         properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
-        properties.setProperty("enable.auto.commit", "true");
-        properties.setProperty("auto.commit.interval.ms", "1000");
-        properties.setProperty("session.timeout.ms", "30000");
+        properties.setProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true");
+        properties.setProperty(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000");
+        properties.setProperty(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "30000");
         properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
         properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, KafkaJsonDeserializer.class.getName());
         return  properties;
     }
 
+    /**
+     *  Close the consumer and shutdown executor
+     */
     public void shutdown() {
         if (consumer != null) {
             consumer.close();
